2c7d4e7f0fd111eafdf64aaf8e9485e1bb10b4df
GabrielCoGa <gabriel.cortes@hotmail.com>
2025-12-04 18:09:39 +0100

    creacion de la base de datos e insercion de los primeros regisgtros

--- version_3/main.py
+++ version_3/main.py
@@ -1,1 +1,27 @@
-from sqlalchemy import column, Integer, String, Foreingnkey, Sequence, Table, MetaData, create_engine
+# SQLAlchemy Tutorial for Beginners
+#https://youtu.be/xr7vDSFXjW0?si=wcwI5RPNsaNZVpJC
+from sqlalchemy import Column, Integer, String, ForeignKey, Sequence, create_engine
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+
+engine  = create_engine('sqlite:///orm.db')
+
+Session = sessionmaker( bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    #id = column(Integer, Sequence('user_id_seq'), primary_key=True)
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    name = Column(String(50))
+    email = Column(String(50))
+
+Base.metadata.create_all(engine)
+
+user1 = User(name='Alice', email='alice@example.com')
+user2 = User(name='Bob', email=' bob@example.com')
+
+session.add_all([user1, user2])
+session.commit()
+

19600054f27840b7de12ea0cac2612563d9765eb
GabrielCoGa <gabriel.cortes@hotmail.com>
2025-12-04 23:30:11 +0100

    todos los ejemplos de sql terminados

--- version_3/mainrelationships.py
+++ version_3/mainrelationships.py
@@ -12,27 +12,27 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
+    #id = column(Integer, Sequence('user_id_seq'), primary_key=True)
     id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
     name = Column(String(50))
     email = Column(String(50))
 
 Base.metadata.create_all(engine)
 
-user1 = User(name='Alice', email='alice@example.com')
-user2 = User(name='Bob', email='bob@example.com')
+#user1 = User(name='Alice', email='alice@example.com')
+#user2 = User(name='Bob', email=' bob@example.com')
 
 #insert data:
-session.add_all([user1, user2])
-session.commit()
+#session.add_all([user1, user2])
+#session.commit()
 
 #query data:
 user = session.query(User).filter_by(name=('Alice')).first()
-if user is not None:
-    print(f'User: {user.name}, Email: {user.email}')
+print(f'User: {user.name}, Email: {user.email}')
 
-    #delete data:
-    session.delete(user)
-    session.commit()
+#delete data:
+session.delete(user)
+session.commit()
 
 #verify deletion
 user = session.query(User).filter_by(name='Alice').first()
@@ -50,8 +50,6 @@ session.commit()
 
 #verify update
 user = session.query(User).filter_by(name='Bob').first()
-if user is not None:
-    print(f'User: {user.name}, Email: {user.email}')
+print(f'User: {user.name}, Email: {user.email}')
 
 
-session.close()
